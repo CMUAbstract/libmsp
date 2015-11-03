ab5e085f0c8c0205bcf75ce5e470ab430c6b9621
Alexei Colin <ac@alexeicolin.com>
2015-11-02 23:36:49 -0500

    memcpy for clang
    The one in the std library has GCC calling convention.

---
+++ src/mem.c
@@ -0,0 +1,11 @@
+#include <stddef.h>
+
+void *memcpy(void *dest, const void *src, size_t n)
+{
+    size_t i = 0;
+    while (i < n) {
+        *((char *)dest + i) = *((char *)src + i);
+        ++i;
+    }
+    return dest;
+}
