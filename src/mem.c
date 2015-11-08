1d0d2cce042cd5155d223552c062b8be7f7a6b7a
Alexei Colin <ac@alexeicolin.com>
2015-11-07 21:38:18 -0500

    add a disabled memset implementation; can't use it

--- src/mem.c
+++ src/mem.c
@@ -9,3 +9,15 @@ void *memcpy(void *dest, const void *src, size_t n)
     }
     return dest;
 }
+
+#if 0 // we cannot override memset because it is called by GCC's C runtime
+void *memset(void *s, int c, size_t n)
+{
+    size_t i = 0;
+    while (i < n) {
+        *((char *)s + i) = (char)c;
+        ++i;
+    }
+    return s;
+}
+#endif
