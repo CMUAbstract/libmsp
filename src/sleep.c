2c75b50479216615d26a66f84672c4959770cef5
Alexei Colin <ac@alexeicolin.com>
2017-05-27 17:03:41 -0400

    sleep: don't wake up on unrelated interrupts

--- src/sleep.c
+++ src/sleep.c
@@ -16,9 +16,15 @@ void msp_sleep(unsigned cycles)
     TIMER_CC(SLEEP_TIMER, LIBMSP_SLEEP_TIMER_CC, CCTL) &= ~CCIFG;
     TIMER_CC(SLEEP_TIMER, LIBMSP_SLEEP_TIMER_CC, CCTL) |= CCIE;
 
+    // The int disable/enable implement the classic check-and-sleep pattern
     __disable_interrupt();
     TIMER(SLEEP_TIMER, CTL) |= MC_1; // start timer
-    __bis_SR_register(LPM4_bits + GIE); // enable interrupt and go to sleep
+
+    while (TIMER_CC(SLEEP_TIMER, LIBMSP_SLEEP_TIMER_CC, CCTL) & CCIE) { // might not be our ISR
+        __bis_SR_register(LPM4_bits + GIE); // enable interrupt and go to sleep
+        __disable_interrupt();
+    }
+    __enable_interrupt();
 }
 
 // We declare the ISR in the library, which disallows sequential multiplexing of this timer.
@@ -28,6 +34,7 @@ __attribute__ ((interrupt(TIMER_VECTOR(LIBMSP_SLEEP_TIMER_TYPE, LIBMSP_SLEEP_TIM
 void  TIMER_ISR(LIBMSP_SLEEP_TIMER_TYPE, LIBMSP_SLEEP_TIMER_IDX, LIBMSP_SLEEP_TIMER_CC) (void)
 {
     TIMER(SLEEP_TIMER, CTL) &= ~MC_1; // stop timer
+    TIMER_CC(SLEEP_TIMER, LIBMSP_SLEEP_TIMER_CC, CCTL) &= ~CCIE; // used as flag that ISR ran
     // CCIFG automatically reset
 
     // We were sleeping waiting for interrupt, so exit sleep
