b5b29dc64d8d66e4c231f1bb39517b87b93c8d84
Alexei Colin <ac@alexeicolin.com>
2017-06-17 13:28:32 -0400

    temp: func for read built-in temp sensor

---
+++ src/include/libmsp/temp.h
@@ -0,0 +1,7 @@
+#ifndef LIBMSP_TEMP_H
+#define LIBMSP_TEMP_H
+
+// Read the built-in temperature sensor
+int msp_sample_temperature();
+
+#endif // LIBMSP_TEMP_H
---
+++ src/temp.c
@@ -0,0 +1,46 @@
+#include <msp430.h>
+
+#include <libio/console.h>
+
+#include "temp.h"
+
+// Table 6-62: temp calibration for 1.2v reference
+#define TLV_CAL30 ((int *)(0x01A1A))
+#define TLV_CAL85 ((int *)(0x01A1C))
+
+// Returns temperature in degrees C (approx range -40deg - 85deg)
+int msp_sample_temperature() {
+  ADC12CTL0 &= ~ADC12ENC;           // Disable conversions
+
+  ADC12CTL3 |= ADC12TCMAP;
+  ADC12CTL1 = ADC12SHP;
+  ADC12CTL2 = ADC12RES_2;
+  ADC12MCTL0 = ADC12VRSEL_1 | BIT4 | BIT3 | BIT2 | BIT1;
+  ADC12CTL0 |= ADC12SHT03 | ADC12ON;
+
+  while( REFCTL0 & REFGENBUSY );
+
+  REFCTL0 = REFVSEL_0 | REFON;
+
+  // Wait for REF to settle
+  // TODO: to use msp_sleep, we need to take the (clock+divider) as arg
+  __delay_cycles(PERIOD_LIBMSP_REF_SETTLE_TIME);
+
+  ADC12CTL0 |= ADC12ENC;                  // Enable conversions
+  ADC12CTL0 |= ADC12SC;                   // Start conversion
+  while (ADC12CTL1 & ADC12BUSY);
+
+  int sample = ADC12MEM0;
+
+  ADC12CTL0 &= ~ADC12ENC;           // Disable conversions
+  ADC12CTL0 &= ~(ADC12ON);  // Shutdown ADC12
+  REFCTL0 &= ~REFON;
+
+  int cal30 = *TLV_CAL30;
+  int cal85 = *TLV_CAL85;
+  int tempC = (sample - cal30) * 55 / (cal85 - cal30) + 30;
+
+  LOG2("[temp] sample=%i => T=%i\r\n", sample, tempC);
+
+  return tempC;
+}
