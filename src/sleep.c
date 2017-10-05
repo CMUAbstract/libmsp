d4afbc77048870cfd18180d260b4d124ec8bd73b
Neil Ryan <soctar@gmail.com>
2017-10-04 22:14:20 -0400

    Added attribute section for sleep timer so we can sleep

--- src/sleep.c
+++ src/sleep.c
@@ -107,3 +107,5 @@ void  TIMER_ISR(LIBMSP_SLEEP_TIMER_TYPE, LIBMSP_SLEEP_TIMER_IDX, LIBMSP_SLEEP_TI
     } // else: we just incremented the tick count
 #endif // LIBMSP_SLEEP_TIMER_TICKS
 }
+__attribute__((section("__interrupt_vector_timer0_b0"),aligned(2)))
+void(*__vector_timer0_b0)(void) = TIMER_ISR(LIBMSP_SLEEP_TIMER_TYPE, LIBMSP_SLEEP_TIMER_IDX, LIBMSP_SLEEP_TIMER_CC);
