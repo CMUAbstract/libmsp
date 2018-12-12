c9985c7095dd476458427692416f69ca87cf5940
kangaruppel <emily.k.ruppel@gmail.com>
2018-12-12 17:13:20 -0500

    added new section that won't get cleared on reboot so we can read out status flags

--- src/include/libmsp/mem.h
+++ src/include/libmsp/mem.h
@@ -4,5 +4,6 @@
 /* The linker script needs to allocate these sections into FRAM region. */
 #define __nv    __attribute__((section(".nv_vars")))
 #define __ro_nv __attribute__((section(".ro_nv_vars")))
+#define __noclear  __attribute__((section(".noclear")))
 
 #endif // _LIBMSP_MEM_H
