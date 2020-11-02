53a7a491fa8a0329942aa33e7ef024e92530d681
kangaruppel <emily.k.ruppel@gmail.com>
2020-11-02 13:35:44 -0500

    bunch of defs for pudu

--- src/include/libmsp/mem.h
+++ src/include/libmsp/mem.h
@@ -2,7 +2,15 @@
 #define _LIBMSP_MEM_H
 
 /* The linker script needs to allocate these sections into FRAM region. */
-#define __nv    __attribute__((section(".nv_vars")))
+//#define __nv    __attribute__((section(".nv_vars")))
 #define __ro_nv __attribute__((section(".ro_nv_vars")))
+
+#define __nv    __attribute__((section(".upper.rodata")))
+
+#define __fram __attribute__((section(".persistent")))
+#define __ro_fram __attribute__((section(".rodata")))
+#define __hifram __attribute__((section(".upper.persistent")))
+#define __ro_hifram __attribute__((section(".upper.rodata")))
+#define __known __attribute__((section(".known")))
 
 #endif // _LIBMSP_MEM_H
